d894d1d543d00c0e14984d4c6354bb76e564269d
antoineleduc <77520817+antoineleduc@users.noreply.github.com>
2021-12-26 11:13:10 -0500

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './img/*.{png}',
+    './img/*.png',
   ],
   theme: {
     extend: {
