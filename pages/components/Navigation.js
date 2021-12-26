d91cc91c7867949a8ccde5663923dc2375000588
Antoine Leduc <antoineleduc.work@gmail.com>
2021-12-26 16:33:40 -0500

    commit

--- pages/components/Navigation.js
+++ pages/components/Navigation.js
@@ -4,7 +4,6 @@ import {
     AtSymbolIcon,
     QuestionMarkCircleIcon
   } from "@heroicons/react/outline"
-import Rellax from "rellax";
 
 function Navigation() {
 
@@ -13,18 +12,22 @@ function Navigation() {
             <section className="flex sticky w-screen h-40 bg-gradient-to-b to-white/0 from-kyndryl justify-center">
                 <div className="flex w-10/12">
                     <div className="flex w-screen justify-center space-x-2 mt-2">
+
                         <button className="flex space-x-2 h-fit text-black bg-white border-gray-500 opacity-70 hover:opacity-80 cursor-pointer rounded-3xl p-4 pr-4 f-sm">
                         <HomeIcon className="h-5 w-5" />
-                        <p>Home</p>
+                        <p id="scrolltext">Home</p>
                         </button>
+
                         <button className="flex space-x-2 h-fit text-black bg-white border-gray-500 opacity-70 hover:opacity-80 cursor-pointer rounded-3xl p-4 pr-4 f-sm">
                         <GlobeIcon className="h-5 w-5" />
                         <p>Directory</p>
                         </button>
+                        
                         <button className="flex space-x-2 h-fit text-black bg-white border-gray-500 opacity-70 hover:opacity-80 cursor-pointer rounded-3xl p-4 pr-4 f-sm">
                         <QuestionMarkCircleIcon className="h-5 w-5" />
                         <p>Help</p>
                         </button>
+                    
                     </div>
                 </div>
             </section>
