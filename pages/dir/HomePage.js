69e2982a4eb152b4798a709ace3cb051d52b0cc3
Antoine Leduc <antoineleduc.work@gmail.com>
2021-12-26 17:10:34 -0500

    commit

--- pages/dir/HomePage.js
+++ pages/dir/HomePage.js
@@ -1,23 +1,20 @@
 import Quote from "../components/Quote.js";
 
-const uri = "http://bluepages.ibm.com/BpHttpApisv3/wsapi?allByNameFuzzy=Antoine%20Leduc"
+const url = "http://bluepages.ibm.com/BpHttpApisv3/wsapi?allByNameFuzzy=Antoine%20Leduc"
 
 function HomePage() {
 
     const QuoteBlock = (
         <div>
             <Quote/>
-            <Quote/>
-            <Quote/>
-            <Quote/>
-            <Quote/>
-            <Quote/>
-            <Quote/>
-            <Quote/>
         </div>
     );
 
-    return QuoteBlock
+    return (
+        
+        QuoteBlock
+        
+        )
 }
 
 export default HomePage
