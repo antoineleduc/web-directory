cb8b833f754beeef72f75ba4837331f62cb0fbb2
antoineleduc <77520817+antoineleduc@users.noreply.github.com>
2021-12-26 09:16:56 -0500

    first get request

--- pages/Center.js
+++ pages/Center.js
@@ -1,14 +1,34 @@
 import Quote from "./Quote";
 
+const uri = "http://bluepages.ibm.com/BpHttpApisv3/wsapi?allByNameFuzzy=Antoine%20Leduc"
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
 function Center() {
 
-    return (
+    const QuoteBlock = (
         <div>
             <Quote/>
             <Quote/>
             <Quote/>
         </div>
-    )
+    );
+
+    const LoggedInUser = (
+        httpGetAsync(uri)
+    );
+
+
+    return QuoteBlock
 }
 
 export default Center
