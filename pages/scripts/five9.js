c5d653c62e78d9d5f5811cd508c2c9269f6a595f
antoineleduc <77520817+antoineleduc@users.noreply.github.com>
2021-12-27 17:55:55 -0500

    Update five9.js

--- pages/scripts/five9.js
+++ pages/scripts/five9.js
@@ -1,4 +1,4 @@
-// "https://api.five9.com/wssupervisor/v3/SupervisorWebService?wsdl%26user=6050328%26password=B34rg6!3Antoine#
+// "https://api.five9.com/wssupervisor/v3/SupervisorWebService?wsdl%26user=***password=***
 
 function five9() {
     return (
