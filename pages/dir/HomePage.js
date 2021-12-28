d3dbec3921799da2e26c9eb413da85302d7a4dc6
Antoine Leduc <antoineleducmusic@gmail.com>
2021-12-28 03:53:04 -0500

    commit

--- pages/dir/HomePage.js
+++ pages/dir/HomePage.js
@@ -7,7 +7,7 @@ function HomePage() {
         <div>
             <Quote/>
             <div className="ml-8">
-            <button onClick={Searchresults} className="flex border-kyndryl-green border-1 border-l-8 bg-kyndryl text-white rounded-xl pl-3 pr-4 pt-2 pb-2">Load Results</button>
+            <button onClick={async ()=> {await fetchURL()}} className="flex border-kyndryl-green border-1 border-l-8 bg-kyndryl text-white rounded-xl pl-3 pr-4 pt-2 pb-2">Load Results</button>
             </div>
         </div>
     );
