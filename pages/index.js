950b6b4bae3b3d65186540c6942047fc592ef657
Antoine Leduc <antoineleducmusic@gmail.com>
2021-12-28 04:02:54 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import HomePage from "./dir/HomePage"
+import HomePage from "../dir/HomePage"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Navigation from "./components/Navigation"
