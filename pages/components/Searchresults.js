9eb679ca22d6a7b46ae8a1a7ab0165f7693f3868
Antoine Leduc <antoineleduc.work@gmail.com>
2021-12-27 20:24:07 -0500

    Update Searchresults.js

--- pages/components/Searchresults.js
+++ pages/components/Searchresults.js
@@ -2,6 +2,7 @@ function Searchresults() {
     const url='https://bluepages.ibm.com/BpHttpApisv3/wsapi?allByNameFuzzy=Antoine%20Leduc';
 
     var XMLHttpRequest = require('xhr2');
+    
     var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
