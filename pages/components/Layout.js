afd70909d72723469d217c36211d3519576c9187
Antoine Leduc <antoineleduc.work@gmail.com>
2021-12-28 12:48:58 -0500

    Update Layout.js

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -11,6 +11,7 @@ const Layout = ({children}) => {
         <div>
             <Header />
             <Navigation />
+            <Results />
             <Quote />
             <Quote />
             <Quote />
